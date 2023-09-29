c28360cb65f339537ee51a7f1636868607d987a3
kaualira <130668344+kaualira@users.noreply.github.com>
2023-09-29 09:01:31 -0300

    kl

---
+++ exercicio1.php
@@ -0,0 +1,31 @@
+<?php
+    $num1 = 2;
+    $num2 = 5;
+
+    if ($num2 > $num1) {
+        echo "Verdadeiro";
+        echo "<br>";
+    } else {
+        echo "Falso";
+        echo "<br>";
+    }
+
+    $robson = 1;
+    $pedro = 2;
+
+    if ($robson != $pedro) {
+        echo "Robson é diferente a Pedro";
+        echo "<br>";
+    } else {
+        echo "Robson é igual de Pedro";
+        echo "<br>";
+    }
+
+    if (12 <= 11) {
+        echo "12 é menor ou igual que 11";
+        echo "<br>";
+    } else {
+        echo "12 não é menor e nem igual a 11";
+        echo "<br>";
+    }
+?>
